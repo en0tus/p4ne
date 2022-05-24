750dc70e3e32d76e82aae6bc6165a5ca312283e0
en0tus <mn.danilin@jet.su>
2022-05-24 18:13:44 +0300

    Lab 1.2

--- Lab 1.2/data_analysis.py
+++ Lab 1.2/data_analysis.py
@@ -12,13 +12,13 @@ column_a=sheet['A'][1:]
 column_c=sheet['C'][1:]
 column_d=sheet['D'][1:]
 
-x_list=map(getvalue, column_a)
-y1_list=map(getvalue, column_c)
-y2_list=map(getvalue, column_d)
+x_list=list(map(getvalue, column_a))
+y1_list=list(map(getvalue, column_c))
+y2_list=list(map(getvalue, column_d))
 
-print(list(x_list))
-print(list(y1_list))
-print(list(y2_list))
+#print(list(x_list))
+#print(list(y1_list))
+#print(list(y2_list))
 
 pyplot.plot(x_list,y1_list,label="Temperature")
 pyplot.plot(x_list,y2_list,label="Sun_Activity")
